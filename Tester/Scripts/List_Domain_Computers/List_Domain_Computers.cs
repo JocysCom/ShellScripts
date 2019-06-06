fc41ab41846702134512aaa1045e31066dc752d7
Evaldas Jocys <evaldas@jocys.com>
2019-06-06 16:25:16 +0100

    Check if server live by querying ports.

--- Tester/Scripts/List_Domain_Computers/List_Domain_Computers.cs
+++ Tester/Scripts/List_Domain_Computers/List_Domain_Computers.cs
@@ -5,12 +5,14 @@ using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 public class List_Domain_Computers
 {
 
-	public static void ProcessArguments(string[] args)
+	public static int ProcessArguments(string[] args)
 	{
 		//for (int i = 0; i < args.Length; i++)
 		//	Console.WriteLine(string.Format("{0}. {1}", i, args[i]));
@@ -28,6 +30,7 @@ public class List_Domain_Computers
 		var maxs = new List<int>();
 		maxs.Add(list.Max(x => string.Format("{0}", x.Name).Length));
 		maxs.Add(list.Max(x => string.Format("{0}", x.Address).Length));
+		maxs.Add(list.Max(x => string.Format("{0}", x.OpenPort).Length));
 		maxs.Add(list.Max(x => string.Format("{0:yyyy-MM-dd HH:mm}", x.LastLogon).Length));
 		maxs.Add(list.Max(x => string.Format("{0}", x.Os).Length));
 		maxs.Add(list.Max(x => string.Format("{0}", x.OsPack).Length));
@@ -44,8 +47,9 @@ public class List_Domain_Computers
 		Write(list, "Domain_Other", true, maxs);
 		Write(list, "Domain_Other", false, maxs);
 		Console.WriteLine();
+		return 0;
 	}
-	
+
 	static bool outputTypeIsCsv = true;
 
 	static void Write(Computer[] list, string file, bool active, List<int> maxs)
@@ -56,14 +60,15 @@ public class List_Domain_Computers
 		var activeList = list.Where(x => x.LastLogon.HasValue && now.Subtract(x.LastLogon.Value) < new TimeSpan(7 * 5, 0, 0, 0, 0)).ToArray();
 		var absentList = list.Except(activeList).ToArray();
 		list = active ? activeList : absentList;
+		UpdateIsOnline(list);
 		for (int i = 0; i < list.Length; i++)
 		{
 			var item = list[i];
 			var m = 0;
 			var format = outputTypeIsCsv
-				? "{0},{1},{2:yyyy-MM-dd HH:mm},{3},{4},{5}\r\n"
-				: "{0,-" + maxs[m++] + "}  {1,-" + maxs[m++] + "}  {2,-" + maxs[m++] + ":yyyy-MM-dd HH:mm}  {3,-" + maxs[m++] + "}  {4,-" + maxs[m++] + "}  {5,-" + maxs[m++] + "}\r\n";
-			sb.AppendFormat(format,	item.Name, item.Address, item.LastLogon, item.Os, item.OsPack, item.OsVersion);
+				? "{0},{1},{2},{3:yyyy-MM-dd HH:mm},{4},{5},{6}\r\n"
+				: "{0,-" + maxs[m++] + "}  {1,-" + maxs[m++] + "} {2,-" + maxs[m++] + "}  {3,-" + maxs[m++] + ":yyyy-MM-dd HH:mm}  {4,-" + maxs[m++] + "}  {5,-" + maxs[m++] + "}  {6,-" + maxs[m++] + "}\r\n";
+			sb.AppendFormat(format, item.Name, item.Address, item.OpenPort, item.LastLogon, item.Os, item.OsPack, item.OsVersion);
 		}
 		System.IO.File.WriteAllText(file + (active ? "_active" : "_absent") + (outputTypeIsCsv ? ".csv" : ".txt"), sb.ToString());
 	}
@@ -131,6 +136,7 @@ public class List_Domain_Computers
 				OsPack = sp,
 				Address = ips,
 				LastLogon = ll,
+				OpenPort = 0,
 			};
 			list.Add(computer);
 			Write(i, all.Length);
@@ -171,8 +177,8 @@ public class List_Domain_Computers
 		public string SamAccountName;
 		public string UserPrincipalName;
 		public DateTime? LastLogon;
+		public int OpenPort;
 	}
-
 
 
 	public static void Write(int i, int max)
@@ -185,6 +191,119 @@ public class List_Domain_Computers
 				Console.Write("\b");
 		Console.Write(s);
 	}
+
+	#region Helper Methods
+
+
+
+	public static bool IsPortOpen(string host, int port, int timeout = 2000, int retry = 1)
+	{
+		var retryCount = 0;
+		while (retryCount < retry)
+		{
+			// Logical delay without blocking the current thread.
+			if (retryCount > 0)
+				System.Threading.Tasks.Task.Delay(timeout).Wait();
+			var client = new System.Net.Sockets.TcpClient();
+			try
+			{
+				var result = client.BeginConnect(host, port, null, null);
+				var success = result.AsyncWaitHandle.WaitOne(timeout);
+				if (success)
+				{
+					client.EndConnect(result);
+					return true;
+				}
+			}
+			catch
+			{
+				// ignored
+			}
+			finally
+			{
+				client.Close();
+				retryCount++;
+			}
+		}
+		return false;
+	}
+
+	static void UpdateIsOnline(Computer computer)
+	{
+		try
+		{
+			CheckNetBios(computer);
+			// RPC
+			if (computer.OpenPort == 0 && IsPortOpen(computer.Name, 135))
+				computer.OpenPort = 135;
+			// RDP
+			if (computer.OpenPort == 0 && IsPortOpen(computer.Name, 3389))
+				computer.OpenPort = 3389;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine("{0} Exception: {1}", computer.Name, ex.Message);
+		}
+	}
+
+	// The following byte stream contains the necessary message
+	// to request a NetBios name from a machine
+	static byte[] NameRequest = new byte[]{
+			0x80, 0x94, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x20, 0x43, 0x4b, 0x41,
+			0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+			0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+			0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x41,
+			0x41, 0x41, 0x41, 0x41, 0x41, 0x00, 0x00, 0x21,
+			0x00, 0x01 };
+
+
+	/// <summary>
+	/// Request NetBios name on UDP port 137. 
+	/// </summary>
+	/// <returns></returns>
+	static void CheckNetBios(Computer computer)
+	{
+		var receiveBuffer = new byte[1024];
+		var requestSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		requestSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 2000);
+		var addressList = Dns.GetHostAddresses(computer.Name);
+		if (addressList.Length == 0)
+		{
+			Console.WriteLine("NetBIOS: {0} host could not be found.", computer.Name);
+			return;
+		}
+		EndPoint remoteEndpoint = new IPEndPoint(addressList[0], 137);
+		var originEndpoint = new IPEndPoint(IPAddress.Any, 0);
+		requestSocket.Bind(originEndpoint);
+		requestSocket.SendTo(NameRequest, remoteEndpoint);
+		try
+		{
+			var receivedByteCount = requestSocket.ReceiveFrom(receiveBuffer, ref remoteEndpoint);
+			if (receivedByteCount >= 90)
+			{
+				var enc = new ASCIIEncoding();
+				var deviceName = enc.GetString(receiveBuffer, 57, 16).Trim();
+				var networkName = enc.GetString(receiveBuffer, 75, 16).Trim();
+				computer.OpenPort = 137;
+				Console.WriteLine("NetBIOS: {0} is online.", deviceName);
+			}
+		}
+		catch (SocketException)
+		{
+			Console.WriteLine("NetBIOS: {0} could not be identified.", computer.Name);
+		}
+	}
+
+	public static void UpdateIsOnline(Computer[] computers)
+	{
+		Parallel.ForEach(computers,
+		new ParallelOptions { MaxDegreeOfParallelism = 10 },
+		   x => UpdateIsOnline(x)
+		);
+	}
+
+	#endregion
 
 }
 
